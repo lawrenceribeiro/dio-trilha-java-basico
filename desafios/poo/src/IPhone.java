d9db94c21b0d2bda8e7707d7a4cf8e15f8e51d1f
lawrenceribeiro <lawrence.ribeiro@hotmail.com>
2025-03-21 10:24:40 -0300

    Enviando desafio POO

---
+++ desafios/poo/src/IPhone.java
@@ -0,0 +1,61 @@
+
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String modelo;
+    private int capacidade;
+
+    public IPhone(String modelo, int capacidade) {
+        this.modelo = modelo;
+        this.capacidade = capacidade;
+    }
+
+    // Métodos do Reprodutor Musical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Tocando a música: " + musica);
+    }
+
+    // Métodos do Aparelho Telefônico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Métodos do Navegador na Internet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+}
+
